cbc935836c13a3bc60f0c80fd9e41b7ed1e3775d
Noam Tal Hod <noam.hod@cern.ch>
2019-10-05 09:31:30 +0300

    added QS1P7

--- CL.py
+++ CL.py
@@ -13,6 +13,9 @@ files = [
          "QS1C_4_L1_31kV_080919r2",   "QS1C_4_L2_31kV_080919r2",   "QS1C_4_L3_31kV_080919r2",   "QS1C_4_L4_31kV_080919r2",
          "QS1C_6_L1_31kV_06092019",   "QS1C_6_L2_31kV_06092019",   "QS1C_6_L3_31kV_06092019",   "QS1C_6_L4_31kV_06092019",
          "QS1C_6_L1_31kV_06092019r2", "QS1C_6_L2_31kV_06092019r2", "QS1C_6_L3_31kV_06092019r2", "QS1C_6_L4_31kV_06092019r2",
+         "QS1P_7_L1_31kV_080919r2",   "QS1P_7_L1_31kV_080919r2",   "QS1P_7_L1_31kV_080919r2",   "QS1P_7_L1_31kV_080919r2",
+         "QS1P_7_L1_31kV_080919",     "QS1P_7_L2_31kV_080919",     "QS1P_7_L3_31kV_080919",     "QS1P_7_L4_31kV_080919",
+         "QS1P_7_L1_31kV_080919r2",   "QS1P_7_L2_31kV_080919r2",   "QS1P_7_L3_31kV_080919r2",   "QS1P_7_L4_31kV_080919r2",
 
 ]
 
@@ -23,8 +26,8 @@ known_channels = {"QS1P":[], "QS1C":[{3:1}, {3:11}]}
 
 # fnames should correspond to existing names found in the files array above
 boards = [
-          {"board":"QS1P12", "fname":"QS1C_6_L1_31kV_06092019"},
-          {"board":"QS1P34", "fname":"QS1C_6_L3_31kV_06092019"},
-          # {"board":"QS1C12", "fname":""},
-          # {"board":"QS1C34", "fname":""},
+          {"board":"QS1P12", "fname":"QS1P_7_L1_31kV_080919"},
+          {"board":"QS1P34", "fname":"QS1P_7_L3_31kV_080919"},
+          {"board":"QS1C12", "fname":"QS1C_6_L1_31kV_06092019"},
+          {"board":"QS1C34", "fname":"QS1C_6_L3_31kV_06092019"},
 ]
